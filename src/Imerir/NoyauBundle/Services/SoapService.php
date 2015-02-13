06ee59ead30daba82aa98a9b135bfe97f89cd896
Knay <payen.john12@gmail.com>
2015-02-13 15:46:51 +0100

    correction erreur fusion

--- src/Imerir/NoyauBundle/Services/SoapService.php
+++ src/Imerir/NoyauBundle/Services/SoapService.php
@@ -10,39 +10,7 @@ class SoapService
     }
 
     public function hello($name) {
-<<<<<<< HEAD
         return 'Bonjour, '.$name;
-    }
-    
-    public function login($username, $passwd) {
-		/*
-    	$csrf_token = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
-    	$now = new \DateTime();
-    	
-    	if( $autologinToken->getExpiresAt() != NULL && $autologinToken->getExpiresAt() < $now ){
-    		return array("csrf_token" => $csrf_token, "last_username" => "", "nextUrl" => $autologinToken->getUrl());
-    	}
-    	
-    	if( $autologinToken->getUser()->getAutologinToken() != $userHash ){
-    		return array("csrf_token" => $csrf_token, "last_username" => "", "error" => "Sorry! There is a problem with your link. Please contact support.");
-    	}
-    	
-    	$user = $autologinToken->getUser();
-    	
-    	$providerKey = $this->container->getParameter('fos_user.firewall_name');
-    	$token = new UsernamePasswordToken($user, null, $providerKey, $user->getRoles());
-    	$this->get('security.context')->setToken($token);
-    	
-    	$event = new InteractiveLoginEvent($request, $token);
-    	$this->get("event_dispatcher")->dispatch("security.authentication", $event);
-		*/
-=======
-    	$utilisateur = $this->container->get('security.context')->getToken()->getUser();
-    	
-    	if ($utilisateur->getRoles()[0] === 'IS_AUTHENTICATED_ANONYMOUSLY')
-        	return 'Bonjour, '.$name;
-        	
-    	return 'KO'; // TODO return soap fault
     }
     
     /**
@@ -52,7 +20,7 @@ class SoapService
      * @return string
      */
     public function login($username, $passwd) {
-    	$dm = $this->get('doctrine.odm.mongodb.document_manager');
+    	/*$dm = $this->get('doctrine.odm.mongodb.document_manager');
     	
 	    $repo = $dm->getRepository('AcmeUserBundle:User');
 	    $user = $repo->findOneByUsername($username);
@@ -67,9 +35,8 @@ class SoapService
 	    $context->setToken($token);
 	
 	    $router = $this->get('router');
-	    $url = $router->generate('dashboard_show');
+	    $url = $router->generate('dashboard_show');*/
     	
     	return "";
->>>>>>> 9de106223a11f576de02ae75c35f52c5d8c5227f
     }
 }
