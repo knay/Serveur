cd09fca509538454532547e4fd15a78b510316a1
Etienne Dijon <etiennedijon@MBP-de-Etienne.imerir.lan>
2015-02-16 11:55:51 +0100

    test

--- src/Imerir/NoyauBundle/Controller/SoapController.php
+++ src/Imerir/NoyauBundle/Controller/SoapController.php
@@ -43,19 +43,25 @@ class SoapController extends ContainerAware
 	 * @Soap\Result(phpType = "string")
 	 */
 	public function loginAction($username,$passwd) {
-		$dm = $this->getDoctrine()->getManager();
+		//$dm = $this->getDoctrine()->getManager();
 
+		$dm = $this->getEntityManager();
+		$queryUser = $dm->createQuery('SELECT username FROM ImerirNoyauBundle:Utilisateur');
 		//requête mango
 		/*
         $repo = $dm->getRepository('AcmeUserBundle:User');
         $user = $repo->findOneByUsername($username);
 		*/
 		//requête mysql
+		/*
 		$user = $this->getDoctrine()->getRepository('ImerirEntity:Utilisateur')->find($username);
+		$hash = hash('sha512',$passwd);
+		$mdp = $this->getDoctrine()->getRepository('ImerirEntity:Utilisateur')->find($hash);
 
-		$mdp = $this->getDoctrine()->getRepository('ImerirEntity:Utilisateur')->find($passwd);
-
-
+		*/
+		$hash = hash('sha512',$passwd);
+		return $hash;
+		/*
         if (!$user && !mdp) {
             throw $this->createNotFoundException('No demouser found!');
         }
@@ -69,5 +75,6 @@ class SoapController extends ContainerAware
         $url = $router->generate('dashboard_show');
 
 		return "";
+		*/
 	}
 }
